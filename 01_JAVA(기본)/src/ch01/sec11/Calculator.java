23a1512db8f57d5c23151733f894e83961c0285d
woojeans7 <thddnwls71@naver.com>
2025-04-16 09:40:45 +0900

    Solve : 01_java 기본 문제 풀이 04-16

---
+++ 01_JAVA(기본)/src/ch01/sec11/Calculator.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // 변수 x에 1 지정, y에 2 배정
+        int x = 1;
+        int y = 2;
+        // 변수 result에 변수 x, y의 합을 저장
+        int result = x + y;
+        // 변수 result를 화면에 출력하세요
+        System.out.println(result);
+    }
+}
