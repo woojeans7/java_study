476fef38eabdbc4fdd8ec90b65d49450133e505a
woojeans7 <thddnwls71@naver.com>
2025-04-22 09:29:07 +0900

    Solve : 04 심화문제 풀이 04-22

---
+++ java/04_JAVA(심화)/src/ch07/exam/HttpServlet.java
@@ -0,0 +1,6 @@
+package ch07.exam;
+
+public abstract class HttpServlet {
+    // 추상 클래스로 등록
+    public abstract void service();
+}
---
+++ java/04_JAVA(심화)/src/ch07/exam/HttpServletExample.java
@@ -0,0 +1,11 @@
+package ch07.exam;
+
+public class HttpServletExample {
+    public static void main(String[] args) {
+        method(new LoginServlet());     // 로그인 합니다.
+        method(new FileDownloadServlet()); // 파일 다운로드합니다.
+    }
+    public static void method(HttpServlet servlet) {
+        servlet.service();
+    }
+}
